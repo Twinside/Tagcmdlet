215e6505e0c49e435de2e3caf6a24373ca6d3647
Vincent Berthoux <twinside@gmail.com>
2010-12-20 18:06:09 +0100

    Adding files to permit loading cmdlet as a powershell 2.0 module

--- Tagcmdlet/uninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿remove-pssnapin tagsnapin
-set-alias installutil $env:windir\Microsoft.NET\Framework\v2.0.50727\installutil
-installutil /uninstall .\Tagcmdlet.dll | out-null
-
-echo "Snapin uninstalled successfully"
-echo "If you've got the line `"Add-pssnapin tagsnapin`" in your profile"
-echo "you should delete it."
-echo ""
